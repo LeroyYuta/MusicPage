910ecea51f0caedc9d0c1ea192518d280ab90231
LeroyYuta <ljuy1999@mail.ru>
2022-09-13 23:11:38 +0500

    added launchpad

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const notes = document.querySelectorAll('.note');
+const pads = document.querySelectorAll('.pad');
 const pianoBlock = document.querySelector('.piano-block');
+const launchpad = document.querySelector('.launchpad');
 
 const playNoteOnKeyboard = (e) => {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
     const note = document.querySelector(`.note[data-key='${e.keyCode}']`);
-
     if (!note) return;
+    note.classList.add('active-note');
+    audio.currentTime = 0;
+    audio.play();
+}
 
-    note.classList.add('active');
+const playPadOnKeyboard = (e) => {
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const pad = document.querySelector(`.pad[data-key='${e.keyCode}']`);
+    if (!pad) return;
+    pad.classList.add('active-pad');
     audio.currentTime = 0;
     audio.play();
 }
@@ -16,19 +25,34 @@ const playNoteOnMouse = (e) => {
     const dataKey = e.target.getAttribute('data-key');
     const note = document.querySelector(`.note[data-key='${dataKey}']`);
     const audio = document.querySelector(`audio[data-key='${dataKey}']`);
-
     if (!note) return;
-
-    note.classList.add('active');
+    note.classList.add('active-note');
     audio.currentTime = 0;
     audio.play();
 }
 
-const removeActive = (e) => {
-    e.target.classList.remove("active");
+const playPadOnMouse = (e) => {
+    const dataKey = e.target.getAttribute('data-key');
+    const audio = document.querySelector(`audio[data-key='${dataKey}']`);
+    const pad = document.querySelector(`.pad[data-key='${dataKey}']`);
+    if (!pad) return;
+
+    pad.classList.add('active-pad');
+    audio.currentTime = 0;
+    audio.play();
 }
 
-notes.forEach(note => note.addEventListener("transitionend", removeActive));
+const removeActiveNote = (e) => {
+    e.target.classList.remove("active-note");
+}
+const removeActivePad = (e) => {
+    e.target.classList.remove("active-pad");
+}
+
+notes.forEach(note => note.addEventListener("transitionend", removeActiveNote));
+pads.forEach(pad => pad.addEventListener("transitionend", removeActivePad));
 
 window.addEventListener('keydown', playNoteOnKeyboard);
+window.addEventListener('keydown', playPadOnKeyboard);
 pianoBlock.addEventListener('click', playNoteOnMouse);
+launchpad.addEventListener('click', playPadOnMouse);
